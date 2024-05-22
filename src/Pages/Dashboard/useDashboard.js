8a0188accbfe362f47e7deaff955cf576cfd1c54
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2024-05-22 12:08:21 +0530

    add demo api

--- src/Pages/Dashboard/useDashboard.js
+++ src/Pages/Dashboard/useDashboard.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../../util/useAxios";
 import { useSelector } from "react-redux";
-
+import FormData from "form-data"
 const useDashboard = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [pendingListOpen, setPendingListOpen] = useState(false);
@@ -31,6 +31,29 @@ const useDashboard = () => {
     axios.get(`${process.env.REACT_APP_BASE_URL}/yuva/list`).then((res) => {
       console.log("res =>", res);
     });
+  }, []);
+
+  useEffect(() => {
+    let data = new FormData();
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://gisem91frd.execute-api.us-east-2.amazonaws.com/test',
+      headers: {
+        'X-Amz-Date': `${process.env.REACT_APP_SECRET_KEY}`,
+        'Authorization': `${process.env.REACT_APP_ACCESS_KEY}`,
+        'Access-Control-Allow-Origin': '*'
+      },
+      data : data
+    };
+
+    axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   }, []);
 
   return {
