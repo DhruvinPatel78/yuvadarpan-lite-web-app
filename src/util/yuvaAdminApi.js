12437862fcf4be356457e8fc4688913955872b23
Dhruvin Patel <patel.dhruvinpatel@gmail.com>
2026-09-23 20:16:40 +0530

    Update API endpoint for getPublicYuva function

--- src/util/yuvaAdminApi.js
+++ src/util/yuvaAdminApi.js
@@ -3,7 +3,7 @@ import axios from "./useAxios";
 export const getPublicYuva = async (id) => {
   try {
     // const response = await axios.get(`/yuvaList/public/${id}`);
-    const response = await axios.get(`/yuvaList/public/${id}`);
+    const response = await axios.get(`/yuvaList/${id}`);
     return response.data;
   } catch (error) {
     throw error;
